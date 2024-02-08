7a6929eb86ad7f5dc1f9d42945bd6b39e4279ec3
Proeung Chiso <122223308+ProeungChiso@users.noreply.github.com>
2024-02-08 08:15:30 +0700

    has debug

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import greeting.Greeting;
 import sum.Sum;
 public class Main {
     public static void main(String[] args) {
-        //Greeting greeting = new Greeting();
-        //greeting.myDisplay();
+        Greeting greeting = new Greeting();
+        greeting.myDisplay();
         Sum sum = new Sum();
         sum.mySum();
     }
--- src/sum/Sum.java
+++ src/sum/Sum.java
@@ -1,18 +1,18 @@
 package sum;
 import java.util.Scanner;
+import java.lang.*;
 public class Sum {
     public void mySum(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("[ Sum Operator ]");
-        int x;
-        int y;
+        int x = 0;
+        int y = 0;
         System.out.print("> Enter x: ");
-        x = Integer.parseInt(scanner.nextLine());
+        x = scanner.nextInt();
         System.out.print("> Enter y: ");
-        y = Integer.parseInt(scanner.nextLine());
+        y = scanner.nextInt();
         System.out.println(STR."Your result: \{x + y}");
     }
-
     public static void main(String[] args) {
         Sum sum = new Sum();
         sum.mySum();
